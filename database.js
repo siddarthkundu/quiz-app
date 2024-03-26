3a6d7449fe0f2377752980c6d1992a31687bcfb8
Siddarth kundu <siddarthkundu19@gmail.com>
2024-03-26 12:31:03 +0530

    updated design styles

---
+++ database.js
@@ -0,0 +1,82 @@
+const questionBank = [
+    {
+        question: "What is the capital city of India ?",
+        option_1: "Bengaluru",
+        option_2: "Kolkata",
+        option_3: "New Delhi",
+        option_4: "Mumbai",
+        correct: "New Delhi"
+    },
+    {
+        question: "Who came second in the FIFA Women's World Cup in 2019 ?",
+        option_1: "Netherlands",
+        option_2: "Australia",
+        option_3: "New Zealand",
+        option_4: "England",
+        correct: " Netherlands"
+    },
+    {
+        question: "What flavour chocolate would you find in a green triangle in a box of Quality Streets ?",
+        option_1: "white Chocolate",
+        option_2: "Dark Chocolate",
+        option_3: "Praline chocolate",
+        option_4: "Ruby Chocolate",
+        correct: " Praline chocolate"
+    },
+    {
+        question: "A body at rest can have?",
+        option_1: "Speed",
+        option_2: "Velocity",
+        option_3: "Momentum",
+        option_4: "Energy",
+        correct: "Velocity"
+    },
+    {
+        question: "Which of the following, can nither be created nor be destroyed ?",
+        option_1: "Force",
+        option_2: "Power",
+        option_3: "Momentum",
+        option_4: "Energy",
+        correct: "Energy"
+    },
+    {
+        question: "Jawaharlal Nehru port is located in ?",
+        option_1: "Kerala",
+        option_2: "Maharashtra",
+        option_3: "Andra Pradesh",
+        option_4: "West Bengal",
+        correct: "Maharashtra"
+    },
+    {
+        question: "Amaravati Stupa is located in which state of india ?",
+        option_1: "Madhya Pradesh",
+        option_2: "Chhattisgarh",
+        option_3: "Andra Pradesh",
+        option_4: "Maharashtra",
+        correct: "Andra Pradesh"
+    },
+    {
+        question: "Which is known as the 'Golden City' ?",
+        option_1: "Jaisalmer",
+        option_2: "Jodhpur",
+        option_3: "Dungarpur",
+        option_4: "Bundi",
+        correct: "Jaisalmer"
+    },
+    {
+        question: "The hill station of Chail is located in ?",
+        option_1: "Himachal Pradesh",
+        option_2: "Uttar Pradesh",
+        option_3: "Madhya Pradesh",
+        option_4: "Arunachal Pradesh",
+        correct: "Himachal Pradesh"
+    },
+    {
+        question:"Red Panda is a state animal of which of the following state ?",
+        option_1: "Mizoram",
+        option_2: "Manipur",
+        option_3: "Meghalaya",
+        option_4: "Sikkim",
+        correct: "Sikkim"
+    }
+]
